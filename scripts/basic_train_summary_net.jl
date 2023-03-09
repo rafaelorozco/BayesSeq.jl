57b093e8ab9dd65751f0db8ebdfc40842d5444eb
Orozco <rorozcom3@cos-4a10679.cos.gatech.edu>
2023-03-08 23:28:39 -0500

    fix sum net train

--- scripts/basic_train_summary_net.jl
+++ scripts/basic_train_summary_net.jl
@@ -1,6 +1,7 @@
 #CUDA_VISIBLE_DEVICES=4 nohup julia scripts/train_openfwi.jl & > cond.out 2> cond.err &
 using DrWatson
-@quickactivate :MultiSourceSummary
+@quickactivate "RosenTrain"
+
 import Pkg; Pkg.instantiate()
 
 using PyPlot
@@ -15,10 +16,11 @@ using DrWatson
 using Images
 using MLUtils
 using UNet
-using FFTW
 using BSON 
-using HDF5
 using SlimPlotting 
+using ProgressMeter: Progress, next!
+using JLD, JLD2
+
 
 function posterior_sampler(G, y, size_x; device=gpu, num_samples=1, batch_size=16)
     # make samples from posterior for train sample 
@@ -73,7 +75,7 @@ function get_loss(G, X_batch, Y_batch; device=gpu, batch_size=16)
 	return l2_total / (num_batches), logdet_total / (num_batches)
 end
 
-sim_name = "cond-simple-openfwi"
+sim_name = "cond-simple-sumnet"
 plot_path = plotsdir(sim_name)
 
 import DrWatson: _wsave
@@ -84,45 +86,45 @@ device = gpu
 
 lr      = 8f-4
 clipnorm_val = 3f0
-noise_lev_x = 0.01f0
+noise_lev_x = 0.005f0
 noise_lev_y = 0.0f0
 
-batch_size   = 8#
-n_epochs     = 150
+batch_size   = 4#
+n_epochs     = 1000
 num_post_samples = 64
 
-validation_perc = 0.9018
+validation_perc = 0.90
 
-save_every   = n_epochs
+save_every   = 100
 plot_every   = 4
 
 sample_i      = 3
 sample_i_test = 1
-n_condmean    = 20
+n_condmean    = 10
 
-Xs_pre = h5open("../openfwi/train_seismic_4_8.h5", "r") do file
-    read(file, "velocity")
-end;
-Ys_pre = h5open("../openfwi/train_seismic_4_8.h5", "r") do file
-    read(file, "data")
-end;
-Xs_pre ./= 1000
-n_src = size(Ys_pre)[3]
-n_total = size(Xs_pre)[end]
-n_c = n_src
-n_in = 1
+data = load("time1_0217.jld");
 
-Xs = zeros(Float32,64,64,1,n_total);
-Ys = zeros(Float32,64,1024,n_src,n_total);
+X = data["X_1"];
+Y = data["Images_1"];
+
+vmin = 0.1
+vmax = 0.7
+
+# Load in training data
+n_total = 128
+validation_perc = 0.9
+
+nx = 256; ny = 256;
+Xs = zeros(Float32, nx, ny, 1, n_total);
+Ys = zeros(Float32, nx, ny, 1, n_total);
 for i in 1:n_total
-	Xs[:,:,1,i] = imresize(Xs_pre[:,:,1,i],(64,64))
-	for j in 1:n_src
-		Ys[:,:,j,i] = imresize(Ys_pre[:,:,j,i],(64,1024))
-	end 
-end 
+    Xs[:,:,:,i] = X[i,end-255:end, end-255:end];
+    Ys[:,:,:,i] = Y[i,end-255:end, 1:256]
+end
 nx, ny = size(Xs)[1:2]
 N = nx*ny
-
+n_c = 1
+n_in = 1
 
 Random.seed!(123);
 (X_train,Y_train), (X_test,Y_test) = splitobs((Xs,Ys); at=validation_perc, shuffle=true);
@@ -137,12 +139,11 @@ n_batches_test = cld(n_test, batch_size)-1
 n_test = batch_size*n_batches_test
 
 # Architecture parametrs
-separate= false #this is separate tracks so that the summary network only works in one scale
 sum_net = true
-h2 = nothing
+h2      = nothing
 unet_lev = 4
 if sum_net
-	h2 = Chain(MaxPool((1,16)),Unet(n_c,n_in,unet_lev))
+	h2 = Chain(Unet(n_c,n_in,unet_lev))
 	trainmode!(h2, true)
 	h2 = FluxBlock(h2)|> device
 end
@@ -158,8 +159,8 @@ G = NetworkConditionalGlow(1, n_in, n_hidden,  L, K; summary_net=h2, split_scale
 G = G |> device;
 
 # Optimizer
-decay = 5f-4
-opt = Flux.Optimiser(WeightDecay(decay),ClipNorm(clipnorm_val), ADAM(lr))
+
+opt = Flux.Optimiser(ClipNorm(clipnorm_val), ADAM(lr))
 
 # Training logs 
 loss   = [];
@@ -171,7 +172,6 @@ loss_test   = [];
 logdet_test = [];
 ssim_test   = [];
 l2_cm_test  = [];
-
 
 for e=1:n_epochs# epoch loop
 	idx_e = reshape(randperm(n_train), batch_size, n_batches) 
@@ -236,12 +236,12 @@ for e=1:n_epochs# epoch loop
 			    fig1 = figure(figsize=(20, 10)); 
 			    y_sum = G.summary_net.forward(y|>device)|>cpu
 
-			    y_plot = y[:,:,1,1]'
+			    y_plot = y[:,:,1,1]
 			    a = quantile(abs.(vec(y_plot)), 98/100)
-			    subplot(1,2,1); imshow(y_plot, vmin=-a,vmax=a,aspect=0.1,interpolation="none", cmap="seismic")
+			    subplot(1,2,1); imshow(y_plot, vmin=-a,vmax=a,aspect=0.5,interpolation="none", cmap="seismic")
 				axis("off");  colorbar(fraction=0.046, pad=0.04); title(L"$y$")
 
-				y_plot = y_sum[:,:,1,1]'
+				y_plot = y_sum[:,:,1,1]
 			    subplot(1,2,2); imshow(y_plot, interpolation="none", cmap="seismic")
 				axis("off");  colorbar(fraction=0.046, pad=0.04); title(L"$h_{\phi}(y)$")
 
@@ -259,39 +259,39 @@ for e=1:n_epochs# epoch loop
 			    ssim_i = round(assess_ssim(X_post_mean[:,:,1,1], x[:,:,1,1]),digits=2)
 			    mse_i = round(mean(error_mean.^2),digits=2)
 
-			    y_plot = y[:,:,1,1]'
+			    y_plot = y[:,:,1,1]
 			    a = quantile(abs.(vec(y_plot)), 98/100)
 
-			    subplot(plot_len,num_cols,(i-1)*num_cols+1); imshow(y_plot, aspect=0.1,vmin=-a,vmax=a,interpolation="none", cmap="seismic")
+			    subplot(plot_len,num_cols,(i-1)*num_cols+1); imshow(y_plot, vmin=-a,vmax=a,interpolation="none", cmap="seismic")
 				colorbar(fraction=0.046, pad=0.04);title(L"$y$")
 			
-			    subplot(plot_len,num_cols,(i-1)*num_cols+2); imshow(X_post[:,:,1,1]', vmin=1.5,vmax=4.5, interpolation="none", cmap="cet_rainbow4")
+			    subplot(plot_len,num_cols,(i-1)*num_cols+2); imshow(X_post[:,:,1,1], vmin=vmin,vmax=vmax, interpolation="none", cmap="cet_rainbow4")
 				axis("off");  colorbar(fraction=0.046, pad=0.04);title("Posterior sample")
 
-				subplot(plot_len,num_cols,(i-1)*num_cols+3); imshow(X_post[:,:,1,2]', vmin=1.5,vmax=4.5, interpolation="none", cmap="cet_rainbow4")
+				subplot(plot_len,num_cols,(i-1)*num_cols+3); imshow(X_post[:,:,1,2], vmin=vmin,vmax=vmax, interpolation="none", cmap="cet_rainbow4")
 				axis("off");  colorbar(fraction=0.046, pad=0.04);title("Posterior sample")
 
 				x_plot = x[:,:,1,1]
-				subplot(plot_len,num_cols,(i-1)*num_cols+4); imshow(x_plot',  vmin=1.5,vmax=4.5,  interpolation="none", cmap="cet_rainbow4")
+				subplot(plot_len,num_cols,(i-1)*num_cols+4); imshow(x_plot,  vmin=vmin,vmax=vmax,  interpolation="none", cmap="cet_rainbow4")
 				axis("off"); title(L"$\mathbf{x_{gt}}$") ; colorbar(fraction=0.046, pad=0.04)
 
-				subplot(plot_len,num_cols,(i-1)*num_cols+5); imshow(X_post_mean[:,:,1,1]' ,  vmin=1.5,vmax=4.5,  interpolation="none", cmap="cet_rainbow4")
+				subplot(plot_len,num_cols,(i-1)*num_cols+5); imshow(X_post_mean[:,:,1,1] ,  vmin=vmin,vmax=vmax,  interpolation="none", cmap="cet_rainbow4")
 				axis("off"); title("Conditional mean SSIM="*string(ssim_i)) ; colorbar(fraction=0.046, pad=0.04)
 
-				subplot(plot_len,num_cols,(i-1)*num_cols+6); imshow(error_mean' , vmin=0,vmax=vmax_error, interpolation="none", cmap="magma")
+				subplot(plot_len,num_cols,(i-1)*num_cols+6); imshow(error_mean , vmin=0,vmax=vmax_error, interpolation="none", cmap="magma")
 				axis("off");title("Mean squared error "*string(mse_i)) ; cb = colorbar(fraction=0.046, pad=0.04)
 				if i ==1 
 					vmax_error = cb.vmax
 				end
 
-				subplot(plot_len,num_cols,(i-1)*num_cols+7); imshow(X_post_std[:,:,1,1]' , vmin=0,vmax=vmax_std,interpolation="none", cmap="magma")
+				subplot(plot_len,num_cols,(i-1)*num_cols+7); imshow(X_post_std[:,:,1,1] , vmin=0,vmax=vmax_std,interpolation="none", cmap="magma")
 				axis("off"); title("Standard deviation") ;cb =colorbar(fraction=0.046, pad=0.04)
 				if i ==1 
 					vmax_std = cb.vmax
 				end
 			end
 			tight_layout()
-		    fig_name = @strdict decay n_c unet_lev nx sum_net clipnorm_val noise_lev_x n_train  e lr n_hidden L K batch_size
+		    fig_name = @strdict  n_c unet_lev nx sum_net clipnorm_val noise_lev_x n_train  e lr n_hidden L K batch_size
 		    if k == 1
 		    	safesave(joinpath(plot_path, savename(fig_name; digits=6)*"_train.png"), fig); close(fig)
 			else
@@ -333,7 +333,7 @@ for e=1:n_epochs# epoch loop
 	    xlabel("Parameter Update") 
 
 		tight_layout()
-		fig_name = @strdict decay n_c unet_lev nx  sum_net clipnorm_val noise_lev_x n_train e lr  n_hidden L K batch_size
+		fig_name = @strdict  n_c unet_lev nx  sum_net clipnorm_val noise_lev_x n_train e lr  n_hidden L K batch_size
 		safesave(joinpath(plot_path, savename(fig_name; digits=6)*"_log.png"), fig); close(fig)
 	end
 	 #save params every 4 epochs
@@ -345,11 +345,11 @@ for e=1:n_epochs# epoch loop
 	        G_save = deepcopy(G);
 	        reset!(G_save.summary_net); # clear params to not save twice
 			Params = get_params(G_save) |> cpu;
-			global save_dict = @strdict decay n_c  unet_lev unet_model n_in nx sum_net clipnorm_val n_train e noise_lev_x lr n_hidden L K Params loss logdet_train l2_cm ssim loss_test logdet_test l2_cm_test ssim_test batch_size; 
+			global save_dict = @strdict  n_c  unet_lev unet_model n_in nx sum_net clipnorm_val n_train e noise_lev_x lr n_hidden L K Params loss logdet_train l2_cm ssim loss_test logdet_test l2_cm_test ssim_test batch_size; 
 		else 
 	        G_save = deepcopy(G);
 			Params = get_params(G_save) |> cpu;
-			global save_dict = @strdict decay n_c unet_lev  n_in nx sum_net clipnorm_val n_train e noise_lev_x lr n_hidden L K Params loss logdet_train l2_cm ssim loss_test logdet_test l2_cm_test ssim_test batch_size; 
+			global save_dict = @strdict  n_c unet_lev  n_in nx sum_net clipnorm_val n_train e noise_lev_x lr n_hidden L K Params loss logdet_train l2_cm ssim loss_test logdet_test l2_cm_test ssim_test batch_size; 
 		end;
 
 		@tagsave(
